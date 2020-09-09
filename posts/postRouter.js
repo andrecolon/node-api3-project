d6a29b3351515ec5776ad7b841989e6f3a00afaa
andre-colon <andrecolon@gmail.com>
2020-09-08 20:33:17 -0700

    added login for both users and post routers return data

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,91 @@
 const express = require('express');
-
+const Posts = require('./postDb')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Posts.get(req.query)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the post',
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  Posts.getById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'post not found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the post',
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+  Posts.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The post has been nuked' });
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the post',
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  Users.update(req.params.id, req.body)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the post',
+      });
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = res.params;
+  Posts.findById(id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        next({ code: 404, message: 'invalid post id' })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      next({ code: 500, message: 'failed to process', err });
+    })
 }
-
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,138 @@
 const express = require('express');
-
+const Users = require('./userDb')
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // do your magic!
+  Users.get(req.query)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the post',
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validatePost, (req, res) => {
+  Users.insert(req.body)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding the post',
+      });
+    });
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get(req.query)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the post',
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  Users.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'post not found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the post',
+      });
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validatePost, (req, res) => {
+  Users.getUserPosts(req.params.id)//Adjust this logic
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'post not found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the post',
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  Users.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The post has been nuked' });
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the post',
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, (req, res) => {
+  Users.update(req.params.id, req.body)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the post',
+      });
+    });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = res.params;
+  Users.findById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        next({ code: 404, message: 'invalid post id' })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      next({ code: 500, message: 'failed to process', err });
+    })
 }
 
 function validateUser(req, res, next) {
